6eafd2f84cae74909f1b6692c61401f8a3d36fc1
Alvin Yang <alvin.yang73@gmail.com>
2020-05-04 18:58:40 -0700

    Updates

---
+++ precache-manifest.8a9df8bb278c032f4bd5d37f4de5edda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28910c92f0a0a26bb9d9d385a890dca0",
+    "url": "/dota-shop-quiz/index.html"
+  },
+  {
+    "revision": "58f04ccc99dcf0d016cc",
+    "url": "/dota-shop-quiz/static/css/main.edc23d94.chunk.css"
+  },
+  {
+    "revision": "37b52e03d53e22400951",
+    "url": "/dota-shop-quiz/static/js/2.d0f82197.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/dota-shop-quiz/static/js/2.d0f82197.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58f04ccc99dcf0d016cc",
+    "url": "/dota-shop-quiz/static/js/main.d4dcb0d5.chunk.js"
+  },
+  {
+    "revision": "af38154a6836701cb60f",
+    "url": "/dota-shop-quiz/static/js/runtime-main.189064bc.js"
+  }
+]);
